2dee208615092025658dd8eee5c4a4af47ffd941
rominasteinberg <rominasteinberg@gmail.com>
2019-05-15 16:31:04 -0400

    estructura de pagina de registro

--- registro.php
+++ registro.php
@@ -1,1 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Seriales - Registro</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="/css/master.css">
+</head>
+<body>
+  <div class="container-fluid">
+    <header>
+      <nav>
+        <img class="logo" src="" alt="">
+        <ul class="menu">
+          <li><a href="#"></a> </li>
+          <li><a href="#"></a> </li>
+          <li><a href="#"></a> </li>
+          <li><a href="#"></a> </li>
+        </ul>
+      </nav>
 
+      <form class="registro" action="jugar.php" method="post">
+        <label for=""></label>
+        <input type="text" name="" value="">
+        <label for=""></label>
+        <input type="text" name="" value="">
+        <label for=""></label>
+        <input type="text" name="" value="">
+        <label for=""></label>
+        <input type="text" name="" value="">
+      </form>
+  </div>
+  </header>
+</body>
+</html>
