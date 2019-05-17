75ac7fa2155b2acfa6dd8de345592f12e0bb28e9
cpinot <cynthia.priscila.pino@gmail.com>
2019-05-17 14:04:29 -0300

    cambios en el archivo preguntas, creacion de carpeta php con archivo header.php, footer.php, baseDatos.php, y adicion de imagenes en carpeta img

---
+++ php/footer.php
@@ -0,0 +1,16 @@
+<?php
+?>
+<footer>
+  <div class="wrapper">
+
+    <nav>
+      <a href="#">Inicio</a>
+      <a href="#">Jugar!</a>
+      <a href="#">Login</a>
+      <a href="#">Registro</a>
+      <a href="#">Preguntas</a>
+    </nav>
+    
+  </div>
+
+</footer>
---
+++ php/header.php
@@ -0,0 +1,18 @@
+<?php
+?>
+<header>
+  <div class="wrapper">
+    <div class="logo">
+        <img src="img/LogoSerialesFB.png" width="70%" alt="Seriales">
+    </div>
+
+    <nav>
+      <a href="#">Inicio</a>
+      <a href="#">Jugar!</a>
+      <a href="#">Login</a>
+      <a href="#">Registro</a>
+      <a href="#">Preguntas</a>
+    </nav>
+  </div>
+  
+</header>
