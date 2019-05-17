92ad56eebc3597eb36aff507859e3d6de1a7c6d1
Aldana Michelino <aldana.michelino@gmail.com>
2019-05-17 08:21:03 -0300

    creación de php de juegos y de mastercss de juegos. creé un css aparte de master para que no nos pisemos el diseño

---
+++ master-master/juego.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
+    <link rel="stylesheet" href="masterjuegos.css">
+
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
+
+    <title>Jugar</title>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <nav>
+        <div class="imagenesnav">
+        <img class="logo" src="img/logo.png" alt="seriales">
+        <img class="banner" src="img/banner1.png" alt="banner">
+      </div>
+
+        <ul class=botonesNav type="none">
+          <li><a href="index1.php">Home</a></li>
+          <li><a href="juego.php">Jugar</a></li>
+          <li><a href="login.php">Log in</a></li>
+          <li><a href="registro.php">Registrate</a></li>
+          <li><a href="preguntas.php">Preguntas</a></li>
+        </ul>
+      </nav>
+
+      <section>
+        <div class="tarjeta">
+          <h2 class="titulo">¿Cuál de las siguientes celebridades </br> no apareción aún en Los Simpson?</h2>
+
+            <div class="pregunta">
+              <article class="celebridad1">
+                <button type="button" name="button">Stephen Hawking</button>
+              </article>
+
+              <article class="celebridad2">
+                <button type="button" name="button">N'sync</button>
+              </article>
+
+              <article class="celebridad3">
+                <button type="button" name="button">Kid Rock</button>
+              </article>
+
+              <article class="celebridad4">
+                <button type="button" name="button">Backstreet Boys</button>
+                </article>
+            </div>
+        </div>
+      </section>
+
+      <footer>
+        <ul class="footer" type="none">
+          <li><a href="#">Preguntas frecuentes</a></li>
+          <li><a href="#">Sobre Seriales</a></li>
+          <li><a href="#">Contactanos</a></li>
+        </ul>
+      </footer>
+
+    </div>
+  </body>
+</html>
