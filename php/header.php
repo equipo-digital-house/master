747ac35febbd5d65f1aed1bcb46714b784054f97
rominasteinberg <rominasteinberg@gmail.com>
2019-05-20 00:40:25 -0300

    boton agregado Jugar

--- php/header.php
+++ php/header.php
@@ -3,11 +3,17 @@
 <header>
   <nav>
     <ul class="row">
-      <li class="col-12 order-1 flex-column order-lg-0 flex-lg-row col-lg-2 menu-items"><a href="index1.php">inicio</a></li>
+      <!-- <li class="col-12 order-1 flex-column order-lg-0 flex-lg-row col-lg-2 menu-items"><a href="index1.php">inicio</a></li>
       <li class="col-12 order-2 flex-column order-lg-1 flex-lg-row col-lg-2 menu-items"><a href="preguntas.php">preguntas</a></li>
       <img class="order-0 offset-3 col-6 order-lg-3 col-lg-2 offset-lg-0 logo" src="img/logo.png" alt="Seriales">
       <li class="col-12 order-4 flex-column order-lg-4 flex-lg-row col-lg-2 menu-items"><a href="registro.php">registro</a> </li>
-      <li class="col-12 order-5 flex-column order-lg-5 flex-lg-row col-lg-2 menu-items"><a href="login.php">login</a> </li>
+      <li class="col-12 order-5 flex-column order-lg-5 flex-lg-row col-lg-2 menu-items"><a href="login.php">login</a> </li> -->
+      <img class="offset-3 col-6 col-lg-2 offset-lg-0 logo" src="img/logo.png" alt="Seriales">
+      <li class="col-12 flex-column flex-lg-row col-lg-2 menu-items"><a href="index1.php">inicio</a></li>
+      <li class="col-12 flex-column flex-lg-row col-lg-2 menu-items"><a href="preguntas.php">preguntas</a></li>
+      <li class="col-12 flex-column flex-lg-row col-lg-2 menu-items"><a href="registro.php">registro</a> </li>
+      <li class="col-12 flex-column flex-lg-row col-lg-2 menu-items"><a href="login.php">login</a> </li>
+      <li class="col-12 flex-column flex-lg-row col-lg-2 menu-items"><a class="play-btn" href="juego.php">jugar</a></li>
     </ul>
   </nav>
 </header>
